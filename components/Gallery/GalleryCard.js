224b361d20b798b329df30bad4e493297e1c0f1f
saphalpantha <saphalpantha@gmail.com>
2023-06-13 10:10:26 +0530

    gallery res

--- components/Gallery/GalleryCard.js
+++ components/Gallery/GalleryCard.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const GalleryCard = ({img}) => {
   return (
-    <div className='fle flex-col   justify-center items-center w-[30rem] h-[30rem]'>
-        <div className='w-[30rem] h-[25rem] bg-[#D9D9D9]'> 
+    <div className='fle flex-col   justify-center items-center w-[20rem] h-[20rem] md:w-[30rem] md:h-[30rem]'>
+        <div className=' w-[20rem] h-[15rem] md:w-[30rem] md:h-[25rem] bg-[#D9D9D9]'> 
                 <img className='w-[100%] h-[100%] bg-cover' src={img} alt="img"></img>
         </div>
         <h1 className='py-4 font-bold bg-[#201F54] text-center w-full text-white' >Album Name</h1>
