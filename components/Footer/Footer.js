e2cdc14901a26f1dc8deff90338e0a695da3d699
saphalpantha <saphalpantha@gmail.com>
2023-06-13 09:51:24 +0530

    footer gap dec

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,7 +5,7 @@ import classes from './Footer.module.css'
 const Footer = () => {
   return (
     <Container>
-      <div className={`max-w-6xl flex flex-col md:max-w-full  items-center justify-center gap-10  pb-[15rem] md:pb-[4rem] pt-[2rem] ${classes.footer}`}>
+      <div className={`max-w-6xl flex flex-col md:max-w-full  items-center justify-center gap-10  pb-[4rem] md:pb-[4rem] pt-[2rem] ${classes.footer}`}>
         <div className="md:max-w-full  flex flex-col md:flex-row  md:justify-between px-0 px-[4rem] py-2  md:py-4 justify-center items-center space-y-6 md:space-y-0  gap-5 md:gap-x-32">
           <div className="max-w-sm flex justify-center items-center flex-col gap-4">
             <h1 className="text-3xl font-bold text-white">About us</h1>
