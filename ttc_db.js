04708b411560d7fba00e8ccc5d7fc24cf48ba710
megaimperialconsultancy <megaimperialconsultancy@gmail.com>
2023-07-26 21:27:56 +0530

    db

--- ttc_db.js
+++ ttc_db.js
@@ -5,10 +5,10 @@ let _db;
 export async function connectToDb() {
 
         const dbConnect = await mysql.createConnection({
-            host:"127.0.0.1",
-            database:"tilottama_edu",
-            user:"tilottama_edu_user",
-            password:"RPvt9OP4uxParxXHzS8Y"
+            host:"db4free.net",
+            database:"ttc_test_db",
+            user:"sulabh_ttc",
+            password:"tilottoma"
     })  
 
     _db = dbConnect
