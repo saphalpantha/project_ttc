5f7926ee58b9f2d4a95aa267f44763c073c7f0c0
saphalpantha <115541533+saphalpantha@users.noreply.github.com>
2023-10-31 16:03:39 +0530

    Update ResultInternal.js

--- components/ResultDetail/ResultInternal.js
+++ components/ResultDetail/ResultInternal.js
@@ -124,7 +124,7 @@ const ResultForInt = () => {
                       required
                       onClick={(e) => setEnteredClass(e.target.value)}
                       className="w-[15rem] h-[2.5rem] border-2 bg-white border-[#201F54] text-black px-[1rem] rounded-full"
-                      value={enteredclass}
+                      value={enteredData.enteredClass}
                     >
                       <option>Select Class</option>
                       <option>11</option>
