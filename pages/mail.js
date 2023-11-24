d5799759e6a3a38b9103f6bacc4ae9b87a138251
saphalpantha <115541533+saphalpantha@users.noreply.github.com>
2023-11-24 16:00:47 +0530

    Update mail.js

--- pages/mail.js
+++ pages/mail.js
@@ -7,7 +7,7 @@ export const getServerSideProps = async ({req,res}) => {
 
       return{
         redirect:{
-          destination:'https://mail.zoho.eu',
+          destination:'https://accounts.zoho.in/signin?servicename=VirtualOffice&signupurl=https://www.zoho.com/mail/zohomail-pricing.html&serviceurl=https://mail.zoho.in',
           permanent:true,
         }
       }
