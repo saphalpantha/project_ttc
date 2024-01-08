839104f4ab2dbd2bee10c121bb4a271d029a8c75
sulabh1919 <esulabh119@gmail.com>
2024-01-08 20:00:09 +0530

    prod fix

--- ttc_db.js
+++ ttc_db.js
@@ -2,20 +2,21 @@ import mysql from "mysql2/promise";
 let _db;
 
 export async function connectToDb() {
-  // const dbConnect = await mysql.createConnection({
-  //     host:"127.0.0.1",
-  //     database:"tilottama_edu",
-  //     user:"tilottama_edu_user",
-  //     password:"RPvt9OP4uxParxXHzS8Y"
-  // })
-
   const dbConnect = await mysql.createConnection({
     host: "127.0.0.1",
-    database: "tilottama",
-    user: "root",
-    password: "",
-    multipleStatements: true,
+    database: "tilottama_edu",
+    user: "tilottama_edu_user",
+    password: "RPvt9OP4uxParxXHzS8Y",
   });
+
+  // const dbConnect = await mysql.createConnection({
+  //   host: "127.0.0.1",
+  //   database: "tilottama",
+  //   user: "root",
+  //   password: "",
+  //   multipleStatements: true,
+  // });
+
   // const dbConnect = await mysql.createConnection({
   //     host:"db4free.net",
   //     database:"ttc_test_db",
